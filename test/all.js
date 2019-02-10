b6826f212c6ff9b832c140a9880fd274971b4caf
Melwyn Furtado <melwynfurtado@gmail.com>
2019-02-10 21:00:09 +0000

    update tests for stricter country validation

--- test/all.js
+++ test/all.js
@@ -4,73 +4,44 @@
  * @author Melwyn Furtado
  */
 
-var assert = require('assert');
-var postcode = require('../lib/index.js');
+const assert = require('assert');
+const postcode = require('../lib/index.js');
 
 // Valid postcodes
 [
-  {
-    code: "10014",
-    country: "US"
-  },
-  {
-    code: "W6 8DL",
-    country: "UK"
-  },
-  {
-    code: "M5P 2N7",
-    country: "CA"
-  },
-  {
-    code: "100-0005",
-    country: "JP"
-  },
-  {
-    code: "100020",
-    country: "INT"
-  },
-  {
-    code: "KFPXWT7D",
-    country: "INT"
-  },
-  {
-    code: "91180-560",
-    country: "INT"
-  },
-  {
-    code: "135",
-    country: "INT"
-  },
-  {
-    code: "SW1A 0AA",
-    country: "UK"
-  }
-].forEach(function (item) {
-  assert.ok(postcode.validate(item.code, item.country), "Valid postcode " + item.code + " for country " + item.country + " was invalid");
+  { code: "10014", country: "US" },
+  { code: "W6 8DL", country: "UK" },
+  { code: "M5P 2N7", country: "CA" },
+  { code: "100-0005", country: "JP" },
+  { code: "100020", country: "INTL" },
+  { code: "KFPXWT7D", country: "INTL" },
+  { code: "91180-560", country: "INTL" },
+  { code: "135", country: "INTL" },
+  { code: "SW1A 0AA", country: "UK" }
+].forEach(function ({ code, country}) {
+  assert.ok(
+    postcode.validate(code, country), 
+    `Valid postcode "${code}" for country "${country} was invalid`
+  );
 });
 
 // Invalid postcodes
 [
-  {
-    code: "!,$^ +@#",
-    country: "INT"
-  },
-  {
-    code: "1234567",
-    country: "UK"
-  },
-  {
-    code: "M5P@2N7",
-    country: "CA"
-  },
-  {
-    code: "M5K3D8",
-    country: "CA"
-  },
-  {
-    code: "100-0005-9088",
-    country: "JP"
-  }
-].forEach(function (item) {
-  assert.ok(!postcode.validate(item.code, item.country), "Invalid postcode " + item.code + " for country " + item.country + " was invalid");
+  { code: "!,$^ +@#", country: "INTL" },
+  { code: "1234567", country: "UK" },
+  { code: "M5P@2N7", country: "CA" },
+  { code: "M5K3D8", country: "CA" },
+  { code: "100-0005-9088", country: "JP" }
+].forEach(function ({ code, country}) {
+  assert.ok(
+    !postcode.validate(code, country), 
+    `Invalid postcode "${code}" for country "${country}" was invalid`
+  );
 });
+
+// Throws error for invalid country
+const invalidCountry = "GB";
+assert.throws(
+  () => postcode.validate("W6 8DL", invalidCountry), 
+  `Country "${invalidCountry}" is not valid!`
+)
